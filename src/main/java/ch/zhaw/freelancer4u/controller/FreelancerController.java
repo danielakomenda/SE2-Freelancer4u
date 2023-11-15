d7f646a6024cb64b14e43926ddf38c371cb88ba5
danielakomenda <d.komenda@hotmail.com>
2023-11-15 13:57:24 +0100

    Get Endpoint für alle Freelancer implementiert

--- src/main/java/ch/zhaw/freelancer4u/controller/FreelancerController.java
+++ src/main/java/ch/zhaw/freelancer4u/controller/FreelancerController.java
@@ -1,8 +1,11 @@
 package ch.zhaw.freelancer4u.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +27,11 @@ public class FreelancerController {
         Freelancer f = freelancerRepository.save(fDAO);
         return new ResponseEntity<>(f, HttpStatus.CREATED);
     }
+
+    @GetMapping("/freelancer")
+    public ResponseEntity<List<Freelancer>> getAllFreelancer() {
+        List<Freelancer> allFree = freelancerRepository.findAll();
+        return new ResponseEntity<>(allFree, HttpStatus.OK);
+    }
+
 }
