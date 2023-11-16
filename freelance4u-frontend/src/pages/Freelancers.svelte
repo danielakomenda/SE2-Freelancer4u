0b6d86a0f2cece2318b62da747cde01ef1d3b32e
danielakomenda <d.komenda@hotmail.com>
2023-11-16 10:54:21 +0100

    Closes #7

---
+++ freelance4u-frontend/src/pages/Freelancers.svelte
@@ -0,0 +1,103 @@
+<script>
+    import axios from "axios";
+
+
+    const api_root = window.location.origin;
+  
+    let freelancers = [];
+    let freelancer = {
+      name: null,
+      email: null,
+    };
+  
+    function getFreelancer() {
+      var config = {
+        method: "get",
+        url: api_root + "/api/freelancer",
+        headers: {},
+      };
+  
+      axios(config)
+        .then(function (response) {
+          freelancers = response.data;
+        })
+        .catch(function (error) {
+          alert("Could not get freelancer");
+          console.log(error);
+        });
+    }
+    getFreelancer();
+  
+    function createFreelancer() {
+      var config = {
+        method: "post",
+        url: api_root + "/api/freelancer",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: freelancer,
+      };
+  
+      axios(config)
+        .then(function (response) {
+          alert("Freelancer created");
+          getFreelancer();
+        })
+        .catch(function (error) {
+          alert("Could not create Freelancer");
+          console.log(error);
+        });
+    }
+  </script>
+  
+  
+  <h1 class="mt-3">Create Freelancer</h1>
+  <form class="mb-5">
+    <div class="row mb-3">
+      <div class="col">
+        <label class="form-label" for="name">Name</label>
+        <input
+          bind:value={freelancer.name}
+          class="form-control"
+          id="name"
+          type="text"
+        />
+      </div>
+    </div>
+    <div class="row mb-3">
+
+      <div class="col">
+        <label class="form-label" for="email">Email</label>
+        <input
+          bind:value={freelancer.email}
+          class="form-control"
+          id="email"
+          type="email"
+        />
+      </div>
+    </div>
+    
+    <button type="button" class="btn btn-primary" on:click={createFreelancer}
+      >Submit</button
+    >
+  </form>
+  
+  <h1>All Freelancers</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">E-Mail</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each freelancers as freelancer}
+        <tr>
+          <td>{freelancer.name}</td>
+          <td>{freelancer.email}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+  
---
+++ freelance4u-frontend/src/pages/Home.svelte
@@ -0,0 +1,11 @@
+<script>
+  import logo from "../assets/svelte.svg";
+</script>
+
+<h1>Welcome to Freelancer4U!</h1>
+
+<!-- Beispiel für die Verwendung von Bildern im Ordner 'assets'. (Import in script beachten.) -->
+<img src={logo} alt="Svelte Logo" /> 
+
+<!-- Beispiel für die Verwendung von Bildern im Ordner 'public' -->
+<img src="/img/vite.svg"  alt="Vite Logo" /> 
